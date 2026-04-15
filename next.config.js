8f22dc62f652f24e04b1c3184bf2774ae0374530
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2026-04-15 22:26:03 +0200

    switch back to webpack
    turbopack error: Uncaught SyntaxError: Failed to execute 'importScripts' on 'WorkerGlobalScope': Identifier 'il' has
    already

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path');
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "onnxruntime-web/all": path.join(__dirname, 'node_modules/onnxruntime-web/dist/ort.all.bundle.min.mjs'),
+        }
+        return config;
+    },
+}
+module.exports = nextConfig
